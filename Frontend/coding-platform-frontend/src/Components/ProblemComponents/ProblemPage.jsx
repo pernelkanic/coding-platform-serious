9a3cf4109bea3ac800f4444aa4a5a3de8faccd81
pernelkanic <whitehattt03@gmail.com>
2024-04-30 21:16:29 +0530

    add code editor component

--- Frontend/coding-platform-frontend/src/Components/ProblemComponents/ProblemPage.jsx
+++ Frontend/coding-platform-frontend/src/Components/ProblemComponents/ProblemPage.jsx
@@ -1,7 +1,31 @@
+import { useAuth } from '@clerk/clerk-react';
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import AceEditor from "react-ace";
+import { useNavigate, useParams } from 'react-router-dom';
+
+import "ace-builds/src-noconflict/ext-language_tools";
+import "ace-builds/src-noconflict/mode-c_cpp";
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/mode-python";
+import "ace-builds/src-noconflict/theme-monokai";
 
 export default function ProblemPage() {
+    const { userId, isLoaded } = useAuth()
+    const navigate = useNavigate()
+
+    
+
+    React.useEffect(() => {
+        if (isLoaded && !userId) {
+            navigate("/login")
+        }
+    }, [isLoaded])
+
+    if (!isLoaded) {
+        window.location.href= '/'
+        return;
+    }
     const[title , settitle] = useState(useParams().id.substring(2,));
     const [probdata , setprobdata] = useState([]);
     const[loading, setLoading] = useState(false);
@@ -34,6 +58,22 @@ export default function ProblemPage() {
                     <p className="mt-9 ml-8">Test Case 0:</p>
                     <p className="ml-12 mt-6">{item.examples.substring(0,35)}</p>
                     <p className="ml-12 mt-6">{item.examples.substring(35)}</p>
+                   <div className=' flex justify-center mt-40 mb-10'>
+                    <AceEditor
+                        height="600px"
+                        width='1000px'
+                        mode="java"
+                        theme="monokai"
+                        fontSize="16px"
+                        highlightActiveLine={true}
+                        setOptions={{
+                            enableLiveAutocompletion: true,
+                            showLineNumbers: true,
+                            tabSize: 2,
+                            }}
+
+                            />
+                            </div>
                   </div>
                 )
             }
